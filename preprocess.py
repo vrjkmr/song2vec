77bf14fe3e8491ea6356fea1c9d1be9bc7c2e70d
Vivian Rajkumar <vivian.rajkumar@hotmail.com>
2020-05-10 00:23:48 +0530

    Added argparse

--- preprocess.py
+++ preprocess.py
@@ -5,63 +5,55 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import skipgrams
 import pickle
 
-# set hyperparameters
-WINDOW_SIZE = 2
-RANDOM_SEED = 42
-BASE_DIR = "test"
+# preprocess function
+def preprocess_data(experiment_name, window_size, random_seed=42):
 
-# load dataframe
-print("loading dataframe...")
-filename = BASE_DIR + "/data/combined_dataset.csv"
-dataframe = pd.read_csv(filename, sep="\t", index_col=0)
+    # load dataframe
+    filename = experiment_name + "/data/combined_dataset.csv"
+    dataframe = pd.read_csv(filename, sep="\t", index_col=0)
 
-# separate track names by playlist
-unique_playlists = dataframe["playlist_name"].unique()
-print("# unique playlists:", len(unique_playlists))
-print("building list of tracks by playlist...")
-tracks_by_playlist = []
-for playlist in unique_playlists:
-    tracks = dataframe[dataframe["playlist_name"] == playlist]["track_full_name"].tolist()
-    tracks_by_playlist.append(tracks)
+    # separate track names by playlist
+    unique_playlists = dataframe["playlist_name"].unique()
+    print("# unique playlists: {count}".format(count=len(unique_playlists)))
+    tracks_by_playlist = []
+    for playlist in unique_playlists:
+        tracks = dataframe[dataframe["playlist_name"] == playlist]["track_full_name"].tolist()
+        tracks_by_playlist.append(tracks)
 
-# tokenize track names
-print("tokenizing tracks...")
-tokenizer = Tokenizer(split=None)
-tokenizer.fit_on_texts(tracks_by_playlist)
-sequences = tokenizer.texts_to_sequences(tracks_by_playlist)
+    # tokenize track names
+    tokenizer = Tokenizer(split=None)
+    tokenizer.fit_on_texts(tracks_by_playlist)
+    sequences = tokenizer.texts_to_sequences(tracks_by_playlist)
 
-# extract vocabulary
-vocabulary_size = len(tokenizer.word_index) + 1
-print("vocabulary size (# tracks):", vocabulary_size)
+    # extract vocabulary
+    vocabulary_size = len(tokenizer.word_index) + 1
+    print("vocabulary size (# tracks): {size}".format(size=vocabulary_size))
 
-# store tokenizer
-print("saving tokenizer...")
-pickle.dump(tokenizer, open(BASE_DIR + "/data/tokenizer.pkl", "wb"))
+    # store tokenizer
+    pickle.dump(tokenizer, open(experiment_name + "/data/tokenizer.pkl", "wb"))
 
-# generate skip-grams
-print("generating negative sampling skip-gram pairs...")
-skip_grams = [skipgrams(sequence, vocabulary_size, window_size=WINDOW_SIZE, seed=RANDOM_SEED) for sequence in sequences]
+    # generate skip-grams
+    skip_grams = [skipgrams(sequence, vocabulary_size, window_size=window_size, seed=random_seed) for sequence in sequences]
 
-# prepare training data
-print("preparing training data...")
-targets = np.array([], dtype="int32")
-contexts = np.array([], dtype="int32")
-y = np.array([], dtype="int32")
+    # prepare training data
+    targets = np.array([], dtype="int32")
+    contexts = np.array([], dtype="int32")
+    y = np.array([], dtype="int32")
 
-for i, skip_gram in enumerate(skip_grams):
-    pairs = list(zip(*skip_gram[0]))
-    if len(pairs) > 0:
-        target_idxs = np.array(pairs[0], dtype="int32")
-        context_idxs = np.array(pairs[1], dtype="int32")
-        labels = np.array(skip_gram[1], dtype="int32")
-        targets = np.concatenate((targets, target_idxs), axis=0)
-        contexts = np.concatenate((contexts, context_idxs), axis=0)
-        y = np.concatenate((y, labels), axis=0)
+    for i, skip_gram in enumerate(skip_grams):
+        pairs = list(zip(*skip_gram[0]))
+        if len(pairs) > 0:
+            target_idxs = np.array(pairs[0], dtype="int32")
+            context_idxs = np.array(pairs[1], dtype="int32")
+            labels = np.array(skip_gram[1], dtype="int32")
+            targets = np.concatenate((targets, target_idxs), axis=0)
+            contexts = np.concatenate((contexts, context_idxs), axis=0)
+            y = np.concatenate((y, labels), axis=0)
 
-X = [targets, contexts]
+    X = [targets, contexts]
 
-# store dataset
-print("saving training data...")
-pickle.dump(X, open(BASE_DIR + "/data/X.pkl", "wb"))
-pickle.dump(y, open(BASE_DIR + "/data/y.pkl", "wb"))
-print("done.")
+    # store dataset
+    num_datapoints = y.shape[0]
+    pickle.dump(X, open(experiment_name + "/data/X.pkl", "wb"))
+    pickle.dump(y, open(experiment_name + "/data/y.pkl", "wb"))
+    print("exported {count} skip-gram pairs to {path}.".format(count=num_datapoints, path=experiment_name + "/data"))
--- train.py
+++ train.py
@@ -1,66 +1,64 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import pickle
 import tensorflow
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Embedding, Flatten, Dot, Dense
+from tensorflow.keras.optimizers import Adam
+import pickle
 
-# set hyperparameters
-RANDOM_SEED = 42
-EMBEDDING_DIM = 100
-NUM_EPOCHS = 20
-BASE_DIR = "test"
+# training function
+def build_and_train_model(experiment_name, embedding_dim, learning_rate, num_epochs, random_seed=42):
 
-# load tokenizer
-print("loading tokenizer...")
-tokenizer = pickle.load(open(BASE_DIR + "/data/tokenizer.pkl", "rb"))
-track2idx = tokenizer.word_index
-vocabulary_size = len(track2idx) + 1
+    # load tokenizer
+    tokenizer = pickle.load(open(experiment_name + "/data/tokenizer.pkl", "rb"))
+    track2idx = tokenizer.word_index
+    vocabulary_size = len(track2idx) + 1
 
-# load training data
-print("loading training data...")
-X = pickle.load(open(BASE_DIR + "/data/X.pkl", "rb"))
-y = pickle.load(open(BASE_DIR + "/data/y.pkl", "rb"))
+    # load training data
+    X = pickle.load(open(experiment_name + "/data/X.pkl", "rb"))
+    y = pickle.load(open(experiment_name + "/data/y.pkl", "rb"))
 
-# set seed
-tensorflow.random.set_seed(RANDOM_SEED)
+    # set seed
+    tensorflow.random.set_seed(random_seed)
 
-# build model architecture
-print("building model...")
-target_inp = Input(shape=(1,))
-target_emb = Embedding(vocabulary_size, EMBEDDING_DIM)(target_inp)
-target_emb = Flatten()(target_emb)
+    # build model architecture
+    target_inp = Input(shape=(1,)) 
+    target_emb = Embedding(vocabulary_size, embedding_dim)(target_inp)
+    target_emb = Flatten()(target_emb)
 
-context_inp = Input(shape=(1,))
-context_emb = Embedding(vocabulary_size, EMBEDDING_DIM)(context_inp)
-context_emb = Flatten()(context_emb)
+    context_inp = Input(shape=(1,))
+    context_emb = Embedding(vocabulary_size, embedding_dim)(context_inp)
+    context_emb = Flatten()(context_emb)
 
-x = Dot(axes=1)([target_emb, context_emb])
-x = Dense(1, activation="sigmoid")(x)
+    x = Dot(axes=1)([target_emb, context_emb])
+    x = Dense(1, activation="sigmoid")(x)
 
-model = Model([target_inp, context_inp], x)
-print(model.summary())
+    model = Model([target_inp, context_inp], x)
+    print(model.summary())
 
-# compile model
-optimizer = "adam"
-loss = "binary_crossentropy"
-model.compile(optimizer=optimizer, loss=loss)
+    # compile model
+    optimizer = Adam(learning_rate=learning_rate)
+    loss = "binary_crossentropy"
+    model.compile(optimizer=optimizer, loss=loss)
 
-# use gpu if available
-device_config = "/CPU:0"
-physical_devices = tensorflow.config.list_physical_devices("GPU")
-if len(physical_devices) > 0:
-    device_config = "/GPU:0"
+    # use gpu if available
+    device_config = "/CPU:0"
+    physical_devices = tensorflow.config.list_physical_devices("GPU")
+    if len(physical_devices) > 0:
+        device_config = "/GPU:0"
 
-# fit model
-print("training model...")
-with tensorflow.device(device_config):
-    r = model.fit(X, y, epochs=NUM_EPOCHS)
+    # fit model
+    with tensorflow.device(device_config):
+        r = model.fit(X, y, epochs=num_epochs, verbose=2)
 
-# store model and embedding weights
-print("done. saving model and embeddings...")
-model.save(BASE_DIR + "/model")
-target_embedding_layer = model.layers[2]
-embedding_weights = target_embedding_layer.get_weights()[0]
-pickle.dump(embedding_weights, open(BASE_DIR + "/embeddings.pkl", "wb"))
-print("done.")
+    # store model
+    filepath = experiment_name + "/model"
+    model.save(filepath)
+    print("exported trained model to {path}.".format(path=filepath))
+
+    # store embedding weights
+    target_embedding_layer = model.layers[2]
+    embedding_weights = target_embedding_layer.get_weights()[0]
+    filepath = experiment_name + "/embeddings.pkl"
+    pickle.dump(embedding_weights, open(filepath, "wb"))
+    print("exported track embeddings to {path}.".format(path=filepath))
