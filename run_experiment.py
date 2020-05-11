4f3cdbab34a4bc6f5d30741cabc092d60ba40240
Vivian Rajkumar <vivian.rajkumar@hotmail.com>
2020-05-11 15:30:18 +0530

    Updated argparse help

--- run_experiment.py
+++ run_experiment.py
@@ -8,13 +8,13 @@ from src.utils import initialize_experiment_directory, write_hyperparameters, cr
 # build argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("experiment_name", help="name of experiment")
-parser.add_argument("--csv_path", help="path to original .csv dataset", default="raw_data/spotify_playlists.csv")
-parser.add_argument("-nr", "--num_rows", type=int, help="number of rows to extract (-1 for whole dataset)", default=10000)
-parser.add_argument("-wsz", "--window_size", type=int, help="(half) window size for skip-gram contexts", default=2)
-parser.add_argument("-emb", "--embedding_dim", type=int, help="word2vec embedding dimension size", default=100)
-parser.add_argument("-e", "--epochs", type=int, help="number of training epochs", default=20)
-parser.add_argument("-lr", "--learning_rate", type=float, help="learning rate for Adam optimizer", default=0.001)
-parser.add_argument("-rs", "--random_seed", type=int, help="random seed for skip-gram generation and training", default=42)
+parser.add_argument("--csv_path", help="path to raw .csv dataset (defaults to raw_data/spotify_playlists.csv)", default="raw_data/spotify_playlists.csv")
+parser.add_argument("-nr", "--num_rows", type=int, help="number of rows to extract (-1 for whole dataset) (defaults to 10000)", default=10000)
+parser.add_argument("-wsz", "--window_size", type=int, help="(half) window size for skip-gram contexts (defaults to 2)", default=2)
+parser.add_argument("-emb", "--embedding_dim", type=int, help="word2vec embedding dimension size (defaults to 100)", default=100)
+parser.add_argument("-e", "--epochs", type=int, help="number of training epochs (defaults to 20)", default=20)
+parser.add_argument("-lr", "--learning_rate", type=float, help="learning rate for Adam optimizer (defaults to 0.001)", default=0.001)
+parser.add_argument("-rs", "--random_seed", type=int, help="random seed for skip-gram generation and training (defaults to 42)", default=42)
 args = parser.parse_args()
 
 # initialize experiment directory
